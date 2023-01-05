b4133de1d88d10eda88fb776cec5d7c9091ee47b
Julia Rudenko <yrudenko@i.ua>
2023-01-05 15:48:37 +0200

    App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+export class App extends Component {
+    state = {
+        images: [],
+        isLoading: false,
+        query: '',
+        error: null,
+        page: 1,
+        showModal: false,
+        largeImageURL: null,
+    };
+
+    // render() {
+
+    // };
+};
